2617ae994d69471b76c8f82a445689d487960831
Adam Shalala <adamshalala@Adams-Air.localdomain>
2023-05-15 14:09:19 +1000

    index for models

---
+++ models/index.js
@@ -0,0 +1,32 @@
+const Category = require('./Category');
+const Product = require('./Product');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE', // optional, ensures that a product cannot exist without a category
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE', 
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE', 
+});
+
+module.exports = { Category, Product, Tag, ProductTag };
