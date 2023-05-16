dc976e2ad4905ffc88f3890668e4b5ddea07120e
Adam Shalala <adamshalala@Adams-Air.localdomain>
2023-05-16 15:02:50 +1000

    fixed seeds files from .js to .json

--- seeds/seed.js
+++ seeds/seed.js
@@ -11,16 +11,16 @@ const tagSeedData = require('./tagSeedData');
 const seedDatabase = async () => {
 
     await sequelize.sync({ force: true });
-
+    console.log('Seeding categories...');
     await Category.bulkCreate(categorySeedData);
-  
+    console.log('Seeding products...');
     await Product.bulkCreate(productSeedData);
-  
+    console.log('Seeding tags...');
     await Tag.bulkCreate(tagSeedData);
-  
+    console.log('Seeding product tags...');
     await ProductTag.bulkCreate(productTagSeedData);
   
-
+    console.log('Seeding completed.');
 
   process.exit(0);
 
